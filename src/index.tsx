9482252ff5d9c1d23845886faa9b11114e490b8b
jsonsivar <jsonsivar@gmail.com>
2020-02-21 11:52:04 -0500

    Init

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,38 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+declare global {
+  interface Window { Ipfs: any; }
+}
+
+
+const runIpfsTests = async () => {
+  // just some tests using the global ipfs object
+  // need to move into utils
+
+  const node = await window.Ipfs.create();
+
+  const uploadResult = await node.add('Hello Terminal '+ Date.now());
+  const hash = (await uploadResult.next()).value.path
+  console.log('uploaded hash: ' + hash);
+
+  const getResult = await node.get(hash);
+  const response = await await getResult.next();
+  const content = await response.value.content;
+  const buffer = await content.next();
+  console.log('got data: ', buffer.value.toString());
+  
+  await node.pin.add(hash);
+
+  for await (const { cid, type } of node.pin.ls()) {
+    console.log({ cid, type })
+  }
+
+  return 'done testing';
+}
+
+runIpfsTests().then(console.log);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
